c507687ccd4c6bfb6ed7fef01f0d6e86a65f28ec
Don-Cryptus <don.cryptus@gmail.com>
2022-06-27 22:26:59 +0200

    fix

--- create.sh
+++ create.sh
@@ -2,9 +2,12 @@
 export "$(grep -vE "^(#.*|\s*)$" .env)"
 
 RUNNERS_FILE="runners.json"
+CURRENT_DIR="$PWD"
 count=`jq '. | length' $RUNNERS_FILE`
 
 for ((i=0; i<$count; i++)); do
+    cd $CURRENT_DIR
+
     repo=`jq -r '.['$i'].repo' $RUNNERS_FILE`
     name=`jq -r '.['$i'].name' $RUNNERS_FILE`
     branch=`jq -r '.['$i'].branch' $RUNNERS_FILE`
