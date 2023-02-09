0ebf8fdc90c5d842ddb932723b1b25f6bca1c8c1
Don-Cryptus <70039285+Don-Cryptus@users.noreply.github.com>
2023-02-09 15:57:22 +0100

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -14,7 +14,7 @@ for ((i=0; i<$count; i++)); do
 
 
 
-    curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/remove-svc.sh | bash -s $repo $name
+    curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/delete.sh | bash -s $repo $name
     if [ -d "$HOME/$name" ]; then rm -Rf "$HOME/$name"; fi
 
 done
