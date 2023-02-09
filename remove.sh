00d1cb7ec2ef995068c4353d02b296c5729fc86d
Don-Cryptus <70039285+Don-Cryptus@users.noreply.github.com>
2023-02-09 16:31:05 +0100

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -12,6 +12,8 @@ for ((i=0; i<$count; i++)); do
     name=`jq -r '.['$i'].name' $RUNNERS_FILE`
     branch=`jq -r '.['$i'].branch' $RUNNERS_FILE`
 
+    if [ -d "$HOME/$name" ]; then cd "$HOME/$name"; fi
+
     curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/remove-svc.sh | bash -s $repo $name || curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/delete.sh | bash -s $repo $name
     if [ -d "$HOME/$name" ]; then rm -Rf "$HOME/$name"; fi
 
