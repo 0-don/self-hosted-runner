d14a6a749d103cf255d28b18a665ad7cd3d62297
don <70039285+don-cryptus@users.noreply.github.com>
2023-11-12 18:52:04 +0100

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -14,9 +14,14 @@ for ((i=0; i < $count; i++)); do
     name=`jq -r '.['$i'].name' $RUNNERS_FILE`
     branch=`jq -r '.['$i'].branch' $RUNNERS_FILE`
 
-    if [ -d "$HOME/$name" ]; then cd "$HOME/$name"; fi
+    # Check if the directory exists one level above the current directory
+    if [ -d "$CURRENT_DIR/../$name" ]; then 
+        cd "$CURRENT_DIR/../$name"
 
-    curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/remove-svc.sh | bash -s $repo $name || curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/delete.sh | bash -s $repo $name
-    if [ -d "$HOME/$name" ]; then rm -Rf "$HOME/$name"; fi
+        # Attempt to remove the service, if it fails try deleting
+        curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/remove-svc.sh | bash -s $repo $name || curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/delete.sh | bash -s $repo $name
 
+        # Remove the directory one level above
+        rm -Rf "$CURRENT_DIR/../$name"
+    fi
 done
